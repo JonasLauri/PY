81f2e7e5c337325613d598f38934a5977664f9ef
jonasllaurinai@gmail.com <jonasllaurinai@gmail.com>
2020-04-17 09:15:35 +0300

    added hello.py

---
+++ hello.py
@@ -0,0 +1,14 @@
+from urllib.request import urlopen
+data = urlopen("https://docs.python.org/3/tutorial/modules.html")
+data_box = []
+for bytes in data:
+    bytes_text = bytes.decode('utf8').split()
+    for text in bytes_text:
+            data_box.append(text)
+
+data.close()
+
+for word in data_box:
+    print(word)
+
+    
