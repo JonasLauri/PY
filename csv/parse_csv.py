81dc9105db38415df427e319bafb8b999b8bd4f6
jonasllaurinai@gmail.com <jonasllaurinai@gmail.com>
2020-05-11 15:28:27 +0300

    csv

---
+++ csv/parse_csv.py
@@ -0,0 +1,16 @@
+import csv
+import os
+
+with open('Per the International Monetary Fund (2019 estimates).csv', 'r') as  csv_file:
+    csv_reader = csv.DictReader(csv_file)
+
+    with open('new_tables.csv', 'w') as new_file:
+        headers = [
+            "Country/Territory",
+            "GDP(US$million)",
+            "Rank"
+        ]
+        csv_writer = csv.DictWriter(new_file, headers, dialect='excel')
+        csv_writer.writeheader()
+        for line in csv_reader:
+            csv_writer.writerow(line)
