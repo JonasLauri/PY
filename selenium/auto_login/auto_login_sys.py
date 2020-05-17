541ba35eaf9d68412c3ac51182aeb2c2c738addc
jonasllaurinai@gmail.com <jonasllaurinai@gmail.com>
2020-05-17 09:23:27 +0300

    changes in auto_login_sys

--- selenium/auto_login/auto_login_sys.py
+++ selenium/auto_login/auto_login_sys.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 # This script makes login automatically at letterboxd website
 from selenium import webdriver
-import sys
+import sys, requests
+url = "https://letterboxd.com/"
 
-USER_NAME = sys.argv[1]
-USER_PASS = sys.argv[2]
+# Status code lookup
+r = requests.get(url)
+r.raise_for_status()
 
 def auto_log(USER_NAME, USER_PASS):
     driver = webdriver.Chrome()
-    driver.get("https://letterboxd.com/")
+    driver.get(url)
     driver.find_element_by_xpath("//a[@class='navlink has-icon']").click()
     driver.find_element_by_id("username").send_keys(USER_NAME)
     driver.find_element_by_id("password").send_keys(USER_PASS)
@@ -16,5 +18,7 @@ def auto_log(USER_NAME, USER_PASS):
     driver.close()
 
 if __name__ == "__main__":
+    USER_NAME = sys.argv[1]
+    USER_PASS = sys.argv[2]
     auto_log(USER_NAME, USER_PASS)
 
