82b5afe6bbbf6e48bef97602cb26b8cb7fd2407a
jonasllaurinai@gmail.com <jonasllaurinai@gmail.com>
2020-05-26 23:03:02 +0300

    mod

--- classes/class.py
+++ classes/class.py
@@ -30,3 +30,15 @@ class Auto:
         args = list(args)
         print(type(args))
         
+class RaceCar:
+    def __init__(self, color, fuel_remaining, **kwargs):
+        laps = 0
+        self.color = color
+        self.fuel_remaining = fuel_remaining
+        for attr, value in kwargs.items():
+            setattr(self, attr, value)
+            
+    def run_lap(self, length):
+        self.fuel_remaining -= (length * 0.125)
+        self.laps += 1
+        
